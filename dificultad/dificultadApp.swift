f1338801336fa492da0fdd2cc700068e38c81bfe
Sandra <sandra.mar05@iest.edu.mx>
2025-09-10 17:42:04 -0600

    Initial Commit

---
+++ dificultad/dificultadApp.swift
@@ -0,0 +1,17 @@
+//
+//  dificultadApp.swift
+//  dificultad
+//
+//  Created by WIN603 on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct dificultadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
